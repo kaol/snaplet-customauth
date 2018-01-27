655edc6d7710db7b519f3aba87ec15c2217be6dc
Kari Pahula <kaol@iki.fi>
2018-01-27 02:25:04 +0200

    Ping db handle before use and acquire a new one if it fails.

--- src/Snap/Snaplet/Hasql.hs
+++ src/Snap/Snaplet/Hasql.hs
@@ -7,6 +7,7 @@ import Control.Concurrent
 import Control.Lens
 import Control.Monad
 import Control.Monad.IO.Class
+import Control.Monad.Trans
 import Control.Monad.Trans.Except
 import Data.ByteString
 import qualified Data.Configurator
@@ -22,22 +23,25 @@ class HasHasql m where
   getHasqlState :: m Hasql
   setHasqlState :: Hasql -> m ()
 
-data Hasql = HasqlSettings (IO (Either C.ConnectionError C.Connection)) (IO ())
+type MaybeConnection = Either C.ConnectionError C.Connection
+
+data Hasql = HasqlSettings (IO MaybeConnection) (MaybeConnection -> IO ()) Int
            | HasqlConnection (IORef (Maybe C.Connection))
-             (IO (Either C.ConnectionError C.Connection)) (IO ())
+             (IO MaybeConnection) (MaybeConnection -> IO ()) Int
 
 hasqlInit :: C.Settings -> SnapletInit b Hasql
 hasqlInit s = makeSnaplet "hasql" "Hasql Snaplet" Nothing $ do
   cfg <- getSnapletUserConfig
-  (poolVar, syncVar) <- liftIO $ do
+  (poolSize, poolVar, syncVar) <- liftIO $ do
     poolSize <- Data.Configurator.lookupDefault 5 cfg "pool"
     poolVar <- newEmptyMVar
     syncVar <- newEmptyMVar
     replicateM_ poolSize $ forkIO $ forever $
       takeMVar syncVar >> (putMVar poolVar =<< C.acquire s)
     replicateM_ poolSize $ putMVar syncVar ()
-    return (poolVar, syncVar)
-  return $ HasqlSettings (takeMVar poolVar) (putMVar syncVar ())
+    return (poolSize, poolVar, syncVar)
+  return $ HasqlSettings (takeMVar poolVar)
+    (\c -> either (const $ return ()) C.release c >> putMVar syncVar ()) poolSize
 
 commit :: S.Session ()
 commit = S.query () $ Q.statement "commit" E.unit D.unit True
@@ -45,20 +49,31 @@ commit = S.query () $ Q.statement "commit" E.unit D.unit True
 begin :: S.Session ()
 begin = S.query () $ Q.statement "begin" E.unit D.unit True
 
+ping :: S.Session Bool
+ping = S.query () $ Q.statement "select true" E.unit (D.singleRow (D.value D.bool)) True
+
 run :: (HasHasql m, MonadIO m) => S.Session a -> m (Either S.Error a)
 run session = do
   state <- getHasqlState
   case state of
-   HasqlConnection ref acquire _ -> liftIO $ runExceptT $ do
+   HasqlConnection ref acquire release poolSize -> liftIO $ runExceptT $ do
+     let tryPool :: Int -> S.Error -> ExceptT S.Error IO C.Connection
+         tryPool 0 = ExceptT . return . Left
+         tryPool n = const $ do
+           c <- liftIO acquire
+           either (\e -> (liftIO $ release c) >> tryPool (n-1) e) (lift . return) =<<
+             (liftIO $ runExceptT $ do
+                 c' <- catchE (ExceptT $ return c) (throwE . S.ClientError)
+                 _ <- ExceptT $ S.run ping c'
+                 return c')
      let initSession = do
-           c <- catchE (ExceptT $ liftIO acquire)
-                (throwE . S.ClientError)
+           c <- tryPool (poolSize+1) (S.ClientError (Just ""))
            liftIO $ writeIORef ref $ Just c
            ExceptT $ liftIO $ S.run begin c
            return c
      c <- maybe initSession return =<< liftIO (readIORef ref)
      ExceptT $ liftIO (S.run session c)
-   HasqlSettings _ _ -> error "connection IORef not initialized"
+   HasqlSettings _ _ _ -> error "connection IORef not initialized"
 
 wrapDbOpen :: (HasHasql (Handler b v)) => Initializer b v ()
 wrapDbOpen = wrapSite bracketDbOpen
@@ -71,17 +86,17 @@ bracketDbOpen :: (HasHasql (Handler b v)) => Handler b v a -> Handler b v a
 bracketDbOpen site = do
   s' <- getHasqlState
   case s' of
-    HasqlSettings s sync ->
+    HasqlSettings s r poolSize ->
       bracketHandler (newIORef Nothing)
-      (\r -> maybeRelease r >> sync)
-      (workSite s sync)
-    HasqlConnection _ _ _ -> error "should not happen"
+      (maybeRelease r)
+      (workSite s r poolSize)
+    HasqlConnection _ _ _ _ -> error "should not happen"
   where
     -- TODO: What to do if commit fails?
-    maybeRelease ref = readIORef ref >>=
-      maybe (return ()) (\c -> S.run commit c >> C.release c)
-    workSite s sync ref = do
-      setHasqlState (HasqlConnection ref s sync)
+    maybeRelease release ref = readIORef ref >>=
+      maybe (return ()) (\c -> S.run commit c >> (release $ Right c))
+    workSite s r poolSize ref = do
+      setHasqlState (HasqlConnection ref s r poolSize)
       x <- site
-      setHasqlState (HasqlSettings s sync)
+      setHasqlState (HasqlSettings s r poolSize)
       return x
